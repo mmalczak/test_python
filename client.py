574e1d7b7200ad255a51aaa46c07bca7c96a1a1a
Milosz Malczak <milosz.malczak@cern.ch>
2020-06-04 16:45:51 +0200

    Using both types of modulation - of the delay as well as of the problem length

--- client.py
+++ client.py
@@ -30,11 +30,12 @@ f = 6
 t = np.array(range(0, length))/length
 
 #delay modulation
-#dm_sig_sin = (np.sin(2 * np.pi * f * t + np.pi / 2) + 1) / 2 / 50
+dm_sig_sin = (np.sin(2 * np.pi * f * t + np.pi / 2) + 1) / 2 / 50
 c = 10
 #dm_sig_square = [1/5 if (el%(length/f)<(length/(c*2*f))) else 0 for el in range(length)]
 dm_sig_square = [0 if (el%(length/f)<((2*c-1)/c)*(length/(2*f))) else 1/5 for el in range(length)]
 
+f = 18
 #problem length modulation
 plm_sig_fft = (np.sin(2 * np.pi * f * t - np.pi / 2) + 1) / 2 * 512
 plm_sig_empty_loop = (np.sin(2 * np.pi * f * t - np.pi / 2) + 1) / 2 * 50000
@@ -44,10 +45,10 @@ plm_sig_random_gen = (np.sin(2 * np.pi * f * t - np.pi / 2) + 1) / 2 * 500
 
 time_diff=0
 for j in range(0, length):
-#    args = [1]+[0]*int(plm_sig_fft[j])
+    args = [1]+[0]*int(plm_sig_fft[j])
 #    args = int(plm_sig_empty_loop[j])
 #    args = int(plm_sig_random_gen[j])
-#    message = pickle.dumps({'task':task, 'args':args})
+    message = pickle.dumps({'task':task, 'args':args})
     start = time.time()
     for i in range(0, num_conn):
         sockets[i].send(message)
@@ -56,6 +57,7 @@ for j in range(0, length):
 #        print(data)
     #time.sleep(dm_sig_sin[j])
     time.sleep(dm_sig_square[j])
+    #time.sleep(0.02)
 
     #time_diff = time.time() - start
     #sleep_time = sig[j] - time_diff
