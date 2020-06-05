6beaa3993c915392249619293dc975129fc760c7
Miłosz Malczak <milosz.malczak@nask.pl>
2020-06-05 15:45:44 +0200

    Moved arrays initialization to seperate function

--- client.py
+++ client.py
@@ -29,36 +29,39 @@ class Client():
         self.control_socket = self.context.socket(zmq.DEALER)
         self.control_socket.connect("tcp://10.10.10.1:"+str(5540))
 
+    def init_arrays(self, num_tasks):
+        t = np.array(range(0, num_tasks))/num_tasks
+        #delay modulation
+        self.dm_sig_sin = (np.sin(2 * np.pi * delay_mod_freq * t + np.pi / 2) + 1) / 2 / 50
+        c = 10
+        #self.dm_sig_square = [1/5 if (el%(num_tasks/delay_mod_freq)<(num_tasks/(c*2*delay_mod_freq))) else 0 for el in range(num_tasks)]
+        self.dm_sig_square = [0 if (el%(num_tasks/delay_mod_freq)<((2*c-1)/c)*(num_tasks/(2*delay_mod_freq))) else 1/5 for el in range(num_tasks)]
+        
+        #problem length modulation
+        self.plm_sig_fft = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 512
+        self.plm_sig_empty_loop = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 50000
+        self.plm_sig_random_gen = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 500
+ 
+
+
     def start(self, num_task, delay_mod_freq, prob_l_freq):
+        self.init_arrays(num_tasks)
+
         ### Energy measurement start ###
         control_message = pickle.dumps({'task':'energy_measure_start', 'args':None})
         self.control_socket.send(control_message)
         data = self.control_socket.recv()
         ### Energy measurement start ###
 
-
-        t = np.array(range(0, num_tasks))/num_tasks
-
-        #delay modulation
-        dm_sig_sin = (np.sin(2 * np.pi * delay_mod_freq * t + np.pi / 2) + 1) / 2 / 50
-        c = 10
-        #dm_sig_square = [1/5 if (el%(num_tasks/delay_mod_freq)<(num_tasks/(c*2*delay_mod_freq))) else 0 for el in range(num_tasks)]
-        dm_sig_square = [0 if (el%(num_tasks/delay_mod_freq)<((2*c-1)/c)*(num_tasks/(2*delay_mod_freq))) else 1/5 for el in range(num_tasks)]
-        
-        #problem length modulation
-        plm_sig_fft = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 512
-        plm_sig_empty_loop = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 50000
-        plm_sig_random_gen = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 500
-        
-        #sig = np.ones(num_tasks)/10000
-        
+        ### Time measurement start ###
         total_time_start = time.time()
+        ### Time measurement start ###
         
         time_diff=0
         for j in range(0, num_tasks):
-            args = [1]+[0]*int(plm_sig_fft[j])
-        #    args = int(plm_sig_empty_loop[j])
-        #    args = int(plm_sig_random_gen[j])
+            args = [1]+[0]*int(self.plm_sig_fft[j])
+        #    args = int(self.plm_sig_empty_loop[j])
+        #    args = int(self.plm_sig_random_gen[j])
             message = pickle.dumps({'task':self.task, 'args':args})
             start = time.time()
             for i in range(0, self.num_conn):
@@ -66,29 +69,26 @@ class Client():
             for i in range(0, self.num_conn):
                 data = self.sockets[i].recv()
         #        print(data)
-            #time.sleep(dm_sig_sin[j])
-        #    time.sleep(dm_sig_square[j])
-        #    time.sleep(0.02)
         
             time_diff = time.time() - start
-            sleep_time = dm_sig_square[j] - time_diff
+            sleep_time = self.dm_sig_square[j] - time_diff
             if sleep_time > 0:
                 time.sleep(sleep_time)
-        #    print("sig")
-        #    print(sig[j])
-        #    print("time")
         #    print(time.time()-start)
 
+        ### Time measurement stop ###
         total_time = time.time() - total_time_start
+        ### Time measurement stop ###
         
         ### Energy measurement stop ###
         control_message = pickle.dumps({'task':'energy_measure_stop', 'args':None})
         self.control_socket.send(control_message)
         energy = self.control_socket.recv()
         energy = float(energy)
+        ### Energy measurement stop ###
+
         print("Energy = {}".format(energy))
         print("Total time = {}".format(total_time))
-        ### Energy measurement stop ###
 
 client = Client()
 
