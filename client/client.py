7d56fe9ea046ca44023ce5e8f18ea76bf86c1fbe
Miłosz Malczak <milosz.malczak@nask.pl>
2020-06-08 14:10:54 +0200

    Seperated function 'stress_server'

--- client/client.py
+++ client/client.py
@@ -10,7 +10,7 @@ class Client():
         self.context = zmq.Context()
         self.num_conn = 4
         self.sockets = []
-        
+
         for i in range(0, self.num_conn):
             port = 5550 + i
             self.sockets.append(self.context.socket(zmq.DEALER))
@@ -21,17 +21,37 @@ class Client():
 
     def init_arrays(self, num_tasks, delay_mod_freq, prob_l_freq):
         t = np.array(range(0, num_tasks))/num_tasks
-        
+
         #delay modulation
         self.dm_sig_sin = (np.sin(2 * np.pi * delay_mod_freq * t + np.pi / 2) + 1) / 2 / 50
         c = 10
         #self.dm_sig_square = [1/5 if (el%(num_tasks/delay_mod_freq)<(num_tasks/(c*2*delay_mod_freq))) else 0 for el in range(num_tasks)]
         self.dm_sig_square = [0 if (el%(num_tasks/delay_mod_freq)<((2*c-1)/c)*(num_tasks/(2*delay_mod_freq))) else 1/5 for el in range(num_tasks)]
-        
+
         #problem length modulation
         self.plm_sig_fft = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 512
         self.plm_sig_empty_loop = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 50000
         self.plm_sig_random_gen = (np.sin(2 * np.pi * prob_l_mod_freq * t - np.pi / 2) + 1) / 2 * 500
+
+    def stress_server(self, task, num_tasks):
+        time_diff=0
+        for j in range(0, num_tasks):
+            args = [1]+[0]*int(self.plm_sig_fft[j])
+        #    args = int(self.plm_sig_empty_loop[j])
+        #    args = int(self.plm_sig_random_gen[j])
+            message = pickle.dumps({'task':task, 'args':args})
+            start = time.time()
+            for i in range(0, self.num_conn):
+                self.sockets[i].send(message)
+            for i in range(0, self.num_conn):
+                data = self.sockets[i].recv()
+        #        print(data)
+
+            time_diff = time.time() - start
+            sleep_time = self.dm_sig_square[j] - time_diff
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+        #    print(time.time()-start)
 
     def time_energy_measurement(self, task, num_tasks, delay_mod_freq, prob_l_freq):
         self.init_arrays(num_tasks, delay_mod_freq, prob_l_freq)
@@ -45,38 +65,22 @@ class Client():
         ### Time measurement start ###
         total_time_start = time.time()
         ### Time measurement start ###
-        
-        time_diff=0
-        for j in range(0, num_tasks):
-            args = [1]+[0]*int(self.plm_sig_fft[j])
-        #    args = int(self.plm_sig_empty_loop[j])
-        #    args = int(self.plm_sig_random_gen[j])
-            message = pickle.dumps({'task':task, 'args':args})
-            start = time.time()
-            for i in range(0, self.num_conn):
-                self.sockets[i].send(message)
-            for i in range(0, self.num_conn):
-                data = self.sockets[i].recv()
-        #        print(data)
-        
-            time_diff = time.time() - start
-            sleep_time = self.dm_sig_square[j] - time_diff
-            if sleep_time > 0:
-                time.sleep(sleep_time)
-        #    print(time.time()-start)
+
+        self.stress_server(task, num_tasks)
 
         ### Time measurement stop ###
         total_time = time.time() - total_time_start
         ### Time measurement stop ###
-        
+
         ### Energy measurement stop ###
         control_message = pickle.dumps({'task':'energy_measure_stop', 'args':None})
         self.control_socket.send(control_message)
         energy = self.control_socket.recv()
         energy = float(energy)
         ### Energy measurement stop ###
+
         return {'energy':energy, 'time':total_time}
-    
+
     def time_energy_stats(self, num_measurements, task, num_tasks, delay_mod_freq, prob_l_mod_freq):
         energy_list = []
         time_list = []
@@ -86,7 +90,7 @@ class Client():
             #print("Total time = {}".format(ret['time']))
             energy_list.append(ret['energy'])
             time_list.append(ret['time'])
-        
+
         plt.scatter(energy_list, time_list)
         plt.show()
 
