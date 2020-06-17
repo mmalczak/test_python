9945007a1db77af4c2d3f6eb72a845fde756e3e8
Milosz Malczak <milosz.malczak@cern.ch>
2020-06-17 18:30:06 +0200

    Added a file with sample configs

---
+++ client/sample_configs.py
@@ -0,0 +1,52 @@
+#task = "fft"
+#num_tasks = 1024
+#dm_freq = 0
+#plm_freq = 64
+#dm_scale = 1 / 50
+#plm_scale = 1
+#num_measurements = 1
+#
+
+#task = "fft"
+#num_tasks = 1024
+#dm_freq = 32
+#plm_freq = 0
+#dm_scale = 3 / 50
+#plm_scale = 1
+#num_measurements = 1
+
+#task = "fft"
+#num_tasks = 1024
+#dm_freq = 12
+#plm_freq = 3
+#dm_scale = 8 / 50
+#plm_scale = 1
+#num_measurements = 1
+
+
+
+#task = "fft"
+#num_tasks = 256
+#dm_freq = 0
+#plm_freq = 16
+#dm_scale = 1 / 50
+#plm_scale = 1
+#num_measurements = 1
+
+
+#task = "fft"
+#num_tasks = 256
+#dm_freq = 8
+#plm_freq = 0
+#dm_scale = 3 / 50
+#plm_scale = 1
+#num_measurements = 1
+
+#task = "fft"
+#num_tasks = 256
+#dm_freq = 12
+#plm_freq = 3
+#dm_scale = 8 / 50
+#plm_scale = 1
+#num_measurements = 1
+
