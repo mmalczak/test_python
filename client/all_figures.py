45afafee472eef002e762fe0e76a18ef4463369a
Miłosz Malczak <milosz.malczak@nask.pl>
2020-07-21 11:25:08 +0200

    Added a script to produce all figures

---
+++ client/all_figures.py
@@ -0,0 +1,6 @@
+print("Producing scatter figures")
+import scatter_figures
+print("Producing adaptive params figures")
+import adaptive_param_figures
+print("Producing mod_vs_tlm figures")
+import mod_vs_tlm_figures
